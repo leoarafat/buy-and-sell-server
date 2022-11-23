9847b32b798f80d568b03620b367438e03b980df
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-11-24 00:15:59 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+//mongodb
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.tjc9clz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+}
+run().catch(console.log);
+
+
 
 app.get('/', async (req, res) => {
     res.send('buy and sell server is running');
