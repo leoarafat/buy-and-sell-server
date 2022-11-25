18a0e2ce473873399da110e7ca4d75edba089aa7
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-11-25 20:23:17 +0600

    impplement user deleted

--- index.js
+++ index.js
@@ -135,9 +135,23 @@ app.put('/users/admin/:id',verifyJWT, async (req, res) => {
   const result = await usersCollection.updateOne(filter, updatedDoc, options);
   res.send(result);
 });
+//user delete
+app.delete('/users/admin/:id', async(req, res)=>{
+  const id = req.params.id;
+  const query = {_id:ObjectId(id)}
+  const result = await usersCollection.deleteOne(query)
+  res.send(result)
+})
+
+
 //visit dashboard
 
-
+app.get('/users/admin/:email', async (req, res) => {
+  const email = req.params.email;
+  const query = { email }
+  const user = await usersCollection.findOne(query);
+  res.send({ isAdmin: user?.role === 'admin' });
+})
 
 
 
